bd6a9cd594cf6151c3f2316e45c62d7ab6743db3
Bhaskar Guha <bhaskarguha012@gmail.com>
2025-03-25 00:59:10 +0530

    some modification

--- BackEnd/routes/messageRoutes.js
+++ BackEnd/routes/messageRoutes.js
@@ -4,3 +4,9 @@ const Database = require("../config/db");
 const sendMessageToAI = require("../services/aiservices");
 
 
+// Route send user message and store response
+
+router.post("/send-message", async (req, res) => {
+    const {message} = req.body;
+})
+
